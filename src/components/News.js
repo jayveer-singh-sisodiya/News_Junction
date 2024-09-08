9038180f39402aa4686942b297ee40cc39560903
Jayveer Sisodiya <jayveersisodiya66@gmail.com>
2024-09-08 23:40:06 +0530

    deployment commit
    site is ready to deploy

--- src/components/News.js
+++ src/components/News.js
@@ -5,16 +5,16 @@ import PropTypes from 'prop-types'
 import imgNewsJunction from "./News Junction (1).png";
 
 export class News extends Component {
-  static defaultProps={
-    country : "us",
+  static defaultProps = {
+    country: "us",
     pageSize: 9,
     category: "general"
   }
 
   static PropType = {
-    country : PropTypes.string,
-    pageSize : PropTypes.number,
-    category :PropTypes.string,
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
   }
 
   constructor() {
@@ -23,61 +23,55 @@ export class News extends Component {
       articles: [],
       loading: true,
       page: 1,
-      totalResults : [],
+      totalResults: [],
     };
   }
 
   async componentDidMount() {
-    let url =
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8bb358f9fd6e46f7bf1da9e42d5a6f82&page=1&pageSize=${this.props.pageSize}`;
-      this.setState({loading : true})
-      let data = await fetch(url);
+    let url = `https://gnews.io/api/v4/top-headlines?category=${this.props.category}&lang=en&country=${this.props.country}&max=${this.props.pageSize}&apikey=5bc1d5bfcbf1097fff7fb655805d3a84`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
     let parseData = await data.json();
     console.log(parseData);
     this.setState({
       articles: parseData.articles,
       totalResults: parseData.totalResults,
-      loading : false
+      loading: false
     });
   }
+
   handleNextClick = async () => {
-    
-        console.log("n");
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8bb358f9fd6e46f7bf1da9e42d5a6f82&page=${
-          this.state.page +1
-        }&pageSize=${this.props.pageSize}`;
-        this.setState({loading : true})
-        let data = await fetch(url);
-        let parseData = await data.json();
-        this.setState
-        ({
-          page: this.state.page + 1,
-          articles: parseData.articles,
-          loading : false 
-        });
-      
+    console.log("n");
+    let url = `https://gnews.io/api/v4/top-headlines?category=${this.props.category}&lang=en&country=${this.props.country}&max=${this.props.pageSize}&apikey=5bc1d5bfcbf1097fff7fb655805d3a84&page=${this.state.page + 1}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      page: this.state.page + 1,
+      articles: parseData.articles,
+      loading: false
+    });
   };
+
   handlePrevClick = async () => {
     console.log("p");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8bb358f9fd6e46f7bf1da9e42d5a6f82&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    this.setState({Spinner : true})
+    let url = `https://gnews.io/api/v4/top-headlines?category=${this.props.category}&lang=en&country=${this.props.country}&max=${this.props.pageSize}&apikey=5bc1d5bfcbf1097fff7fb655805d3a84&page=${this.state.page - 1}`;
+    this.setState({ loading: true });
 
     let data = await fetch(url);
     let parseData = await data.json();
     this.setState({
       page: this.state.page - 1,
       articles: parseData.articles,
-      loading : false
+      loading: false
     });
   };
 
   render() {
     return (
       <div className="container my-3">
-        <h2 className="text-center">Top HEadlines</h2>
-        {this.state.loading && <Spinner></Spinner>}
+        <h2 className="text-center">Top Headlines</h2>
+        {this.state.loading && <Spinner />}
         <div className="row">
           {!this.state.loading && this.state.articles.map((element) => {
             return (
@@ -85,9 +79,9 @@ export class News extends Component {
                 <NewsItem
                   title={element.title}
                   discription={element.description}
-                  imgUrl={element.urlToImage || imgNewsJunction}
+                  imgUrl={element.image || imgNewsJunction}
                   newsUrl={element.url}
-                ></NewsItem>
+                />
               </div>
             );
           })}
@@ -99,13 +93,12 @@ export class News extends Component {
             className="btn btn-outline-primary mx-1"
             onClick={this.handlePrevClick}
           >
-            {" "}
             &larr; Previous
           </button>
           <button
-            disabled={this.state.page+1 > Math.ceil(this.state.totalResults / this.props.pageSize)}
+            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)}
             type="button"
-            className="btn btn-outline-primary "
+            className="btn btn-outline-primary"
             onClick={this.handleNextClick}
           >
             Next &rarr;
